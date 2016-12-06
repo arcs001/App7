2e1714cad807c2485961ac6983264e762ea1f037
Alexandre Campos <alexde@microsoft.com>
2016-12-06 14:20:40 -0200

    .

---
+++ App7/App.cs
@@ -0,0 +1,61 @@
+﻿using MonoTouch.Foundation;
+using MonoTouch.MediaPlayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App7
+{
+    public class App : Application
+    {
+        public App()
+        {
+            Button button = new Button
+            {
+                Text = "Click Me!",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            button.Clicked += OnButtonClicked;
+
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        button
+                    }
+                }
+            };
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+
+            MPMusicPlayerController c = new MPMusicPlayerController();
+            c.SkipToNextItem();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
